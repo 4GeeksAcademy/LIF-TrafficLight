e9f0f5095d4c262eb71a28ab41ea45bd52a9e545
Campeones1950 <168790271+Campeones1950@users.noreply.github.com>
2024-10-08 20:44:24 +0000

    LIF Proyecto actualiazo

---
+++ src/js/component/TrafficLigth.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+
+export const TrafficLigth = () => {
+    const[activeLight, setActiveLight]= useState(null);
+    return (
+        
+            <div className="bg-dark d-flex flex-column justify-content-around align-items-center"
+
+            style={{ 
+                width:"100px", 
+                height:"300px",
+                borderRadius:"20px",
+                padding:"10px",
+                               }}>
+
+                <button type="button" className="btn" 
+                onClick={()=> setActiveLight ("red", alert("Detengase"))}
+
+                style={{
+                    borderRadius:"50%",
+                    height:"70px",
+                    width:"70px",
+                    backgroundColor: activeLight === "red"? "red": "#7e0000",
+                    }}>R
+                    </button>
+                <button type="button" className="btn" 
+                 onClick={()=> setActiveLight ("yellow", alert("Más lento apuradito"))}
+              
+                style={{
+                    borderRadius:"50%",
+                    height:"70px",
+                    width:"70px",
+                    backgroundColor: activeLight === "yellow"? "yellow": "#7e7e00",
+                    }}>Y</button>
+                <button type="button" className="btn"
+                onClick={()=> setActiveLight ("green", alert("Puede avanzar, pero despacito eee!!."))}
+
+                style={{
+                    borderRadius:"50%",
+                    height:"70px",
+                    width:"70px",
+                    backgroundColor: activeLight === "green"? "green": "#004d00",
+
+                    }}>G</button>
+
+           </div>
+           
+           
+
+    )
+}
